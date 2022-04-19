cab9957a33c5eed5387f342016699ee74c5231f1
mvanmaele <mvanmaele@talusanalytics.com>
2022-04-19 14:59:07 -0400

    Add feature to join GDP and disb/GDP ratio

--- analyses/Top 30 country funder disbursed normalized by GDP per capita/code/analysis.sql
+++ analyses/Top 30 country funder disbursed normalized by GDP per capita/code/analysis.sql
@@ -1,7 +1,23 @@
 SELECT
     v.*,
     v."Stakeholder name" IN ('Norway', 'Denmark', 'Sweden', 'Finland') AS "Nordic?",
-    tf."Total disbursed for capacity (USD, nominal)"
+    tf."Total disbursed for capacity (USD, nominal)",
+    stg.gdp AS "GDP per capita in current USD for 2020",
+    CASE
+        WHEN (
+            stg.gdp > 0
+            AND stg.gdp IS NOT NULL
+        )
+        AND (
+            tf."Total disbursed for capacity (USD, nominal)" IS NOT NULL
+        ) THEN (
+            tf."Total disbursed for capacity (USD, nominal)" / stg.gdp
+        )
+        ELSE NULL
+    END AS "Ratio of disbursed and GDP"
 FROM
     venn_final v
     LEFT JOIN top_funders tf ON tf."Funder ID" = v."Stakeholder ID"
+    LEFT JOIN "research".stakeholders_to_gdp stg ON stg.stakeholder_id = v."Stakeholder ID"
+ORDER BY
+    "Ratio of disbursed and GDP"
