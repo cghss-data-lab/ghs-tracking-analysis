33b1c1e085e39771d424ff6acc4d4b4108244c0f
mvanmaele <mvanmaele@talusanalytics.com>
2022-04-19 13:00:54 -0400

    Store latest results and `by-date` results separately

--- util/copy.sh
+++ util/copy.sh
@@ -1,5 +1,7 @@
 fn=$(basename "$(dirname "$PWD"/..)")__$(date).csv
+fn_latest=$(basename "$(dirname "$PWD"/..)")__latest.csv
 sql=$(cat $1)
 echo $sql;
-mkdir results;
-psql service=tracking-analysis -c """\copy ($sql) to './results/$fn' csv header;""";
+mkdir -p results/by-date;
+psql service=tracking-analysis -c """\copy ($sql) to './results/by-date/$fn' csv header;""" && \
+cp "./results/by-date/$fn" "./results/$fn_latest";
