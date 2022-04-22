510aa9e2017948523b3da11f14e9b1d8242bc368
mvanmaele <mvanmaele@talusanalytics.com>
2022-04-22 17:56:50 -0400

    Fix typo

--- analyses/Funders by PHEIC by geographic region by funder category/run.sh
+++ analyses/Funders by PHEIC by geographic region by funder category/run.sh
@@ -4,5 +4,5 @@ origdir=$(pwd)
 bash ./code/build.sh &&
     cd "$scriptdir" &&
     bash ../../util/copy.sh ./code/analysis.sql "Funders by PHEIC by geographic region by funder category" &&
-    bash ../../util/copy.sh ./code/analysis-agg_by_region.sql "Amound funded by PHEIC by WHO region" &&
+    bash ../../util/copy.sh ./code/analysis-agg_by_region.sql "Amount funded by PHEIC by WHO region" &&
     cd "$origdir" || exit
