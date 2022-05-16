a9a98a1d4580768add8911173a586725f75fbd03
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2022-05-16 18:19:29 -0400

    Add code for analysis

--- analyses/Funders by core capacity by funder category/code/analysis.sql
+++ analyses/Funders by core capacity by funder category/code/analysis.sql
@@ -1,49 +1,53 @@
-WITH top_level_stakeholder_ids AS (
-    SELECT
-        DISTINCT parent_id
-    FROM
-        children_to_parents_direct_credit
-    WHERE
-        parent_id NOT IN (
-            SELECT
-                DISTINCT child_id
-            FROM
-                children_to_parents_direct_credit
-            WHERE
-                parent_id != child_id
-        )
-)
-SELECT
-    tf."Funder name" AS "Funder name (philanthropies only, from among top 50 only)",
-    tf."Funder ID",
-    sum(sf.value) AS "Total disb. to countries post-2013 for cap. (nom. USD)"
-FROM
-    top_funders tf
-    JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON ftsodc.stakeholder_id = tf."Funder ID"
-    JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON ftstdc.flow_id = ftsodc.flow_id
-    JOIN stakeholders target ON target.id = ftstdc.stakeholder_id
-    JOIN simple_flows sf ON sf.sf_id = ftsodc.flow_id
-WHERE
-    tf."Category (country, international, or philanthropy)" = 'Philanthropy'
-    AND target.sankey_cat = 'Country'
-    AND tf."Funder ID" IN (
-        SELECT
-            *
-        FROM
-            top_level_stakeholder_ids
-    )
-    AND target.id IN (
-        SELECT
-            *
-        FROM
-            top_level_stakeholder_ids
-    )
-    AND sf.flow_type = 'disbursed_funds'
-    AND sf.response_or_capacity = 'capacity'
-    AND sf."year" BETWEEN 2014
-    AND 3000
-GROUP BY
-    tf."Funder name",
-    tf."Funder ID"
-ORDER BY
-    "Total disb. to countries post-2013 for cap. (nom. USD)" DESC
+as with top_level_stakeholder_ids as (
+select
+	distinct children_to_parents_direct_credit.parent_id
+from
+	children_to_parents_direct_credit
+where
+	not (children_to_parents_direct_credit.parent_id in (
+	select
+		distinct children_to_parents_direct_credit_1.child_id
+	from
+		children_to_parents_direct_credit children_to_parents_direct_credit_1
+	where
+		children_to_parents_direct_credit_1.parent_id <> children_to_parents_direct_credit_1.child_id))
+        ),
+results as (
+select
+	s.cat_analysis AS "Funder analysis category",	
+	c.fullname as "Core capacity",
+	c.name as "Core capacity code",
+	sum(sf.value) as "Total disbursed (nominal USD)"
+from
+	stakeholders s
+join flows_to_stakeholder_origins_direct_credit ftsodc on
+	ftsodc.stakeholder_id = s.id
+join simple_flows sf on
+	sf.sf_id = ftsodc.flow_id
+join ccs_to_flows ccstf on
+	ccstf.cc_flow_id = sf.sf_id
+join core_capacities c on
+	c.id = ccstf.cc_id
+where
+	sf.flow_type = 'disbursed_funds'::flow_types
+	and sf.response_or_capacity = 'response'::response_or_capacity_vals
+	and (s.id in (
+	select
+		top_level_stakeholder_ids.parent_id
+	from
+		top_level_stakeholder_ids))
+group by
+	c.fullname,
+	c.name,
+	s.cat_analysis 
+order by
+	c.fullname, (sum(sf.value)) desc
+	)
+select
+	results."Funder analysis category",
+	results."Core capacity",
+	results."Core capacity code",
+	results."Total disbursed (nominal USD)",
+	round((results."Total disbursed (nominal USD)" / sum(results."Total disbursed (nominal USD)") over (order by results."Core capacity"))::numeric * 100.0, 2) as "Percentage of disbursements for this Core capacity"
+from
+	results;
