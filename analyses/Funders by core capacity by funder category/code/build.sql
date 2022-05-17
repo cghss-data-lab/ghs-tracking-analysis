f2f5cae99911b35e7e4527a8088564ecade31bc0
mvanmaele <mvanmaele@talusanalytics.com>
2022-05-17 10:16:00 -0400

    Add separate `build.sql` to create view

---
+++ analyses/Funders by core capacity by funder category/code/build.sql
@@ -0,0 +1,65 @@
+CREATE
+OR REPLACE VIEW public.core_capacity_funders_by_category AS WITH top_level_stakeholder_ids AS (
+    SELECT
+        DISTINCT children_to_parents_direct_credit.parent_id
+    FROM
+        children_to_parents_direct_credit
+    WHERE
+        NOT (
+            children_to_parents_direct_credit.parent_id IN (
+                SELECT
+                    DISTINCT children_to_parents_direct_credit_1.child_id
+                FROM
+                    children_to_parents_direct_credit children_to_parents_direct_credit_1
+                WHERE
+                    children_to_parents_direct_credit_1.parent_id <> children_to_parents_direct_credit_1.child_id
+            )
+        )
+),
+results AS (
+    SELECT
+        s.cat_analysis AS "Funder analysis category",
+        c.fullname AS "Core capacity",
+        c.name AS "Core capacity code",
+        sum(sf.value) AS "Total disbursed (nominal USD)"
+    FROM
+        stakeholders s
+        JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON ftsodc.stakeholder_id = s.id
+        JOIN simple_flows sf ON sf.sf_id = ftsodc.flow_id
+        JOIN ccs_to_flows ccstf ON ccstf.cc_flow_id = sf.sf_id
+        JOIN core_capacities c ON c.id = ccstf.cc_id
+    WHERE
+        sf.flow_type = 'disbursed_funds' :: flow_types
+        AND sf.response_or_capacity = 'response' :: response_or_capacity_vals
+        AND (
+            s.id IN (
+                SELECT
+                    top_level_stakeholder_ids.parent_id
+                FROM
+                    top_level_stakeholder_ids
+            )
+        )
+    GROUP BY
+        c.fullname,
+        c.name,
+        s.cat_analysis
+    ORDER BY
+        c.fullname,
+        (sum(sf.value)) DESC
+)
+SELECT
+    results."Funder analysis category",
+    results."Core capacity",
+    results."Core capacity code",
+    results."Total disbursed (nominal USD)",
+    round(
+        (
+            results."Total disbursed (nominal USD)" / sum(results."Total disbursed (nominal USD)") over (
+                ORDER BY
+                    results."Core capacity"
+            )
+        ) :: numeric * 100.0,
+        2
+    ) AS "Percentage of disbursements for this Core capacity"
+FROM
+    results
--- analyses/Funders by core capacity by funder category/run.sh
+++ analyses/Funders by core capacity by funder category/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 scriptdir=$(dirname "$0")
 origdir=$(pwd)
+
+# run build SQL file if it exists, using `run_sql_file_with_service.sh` to run on the
+# database defined in `run_sql_file_with_service`
+[[ -f ./code/build.sql ]] &&
+    (bash ../../util/run_sql_file_with_service.sh ./code/build.sql || exit 1)
+
+# run analysis SQL file, using `copy.sh` to write CSV outputs
 cd "$scriptdir" &&
     bash ../../util/copy.sh ./code/analysis.sql &&
     cd "$origdir" || exit 1
