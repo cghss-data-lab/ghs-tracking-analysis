7d6001367543c4aee14be6314b2a58ad07784163
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2023-11-03 12:39:19 -0400

    Update analysis.sql

--- analyses/Funders and their core capacity subtotals/code/analysis.sql
+++ analyses/Funders and their core capacity subtotals/code/analysis.sql
@@ -1,2 +1,29 @@
-SELECT
-    1 AS "first_column"
+SELECT ce.fullname AS "ce",
+    cc.fullname AS "core capacity",
+	s.name AS "funder",
+	s.subcat as "funder category",
+	sf.year as "year",
+    round(sum(sf.value)) AS "value"
+FROM simple_flows sf,
+    flows_to_stakeholder_origins_direct_credit ftsodc,
+    ccs_to_flows ctf,
+    cc_to_ce ctc,
+    core_elements ce,
+    core_capacities cc,
+	stakeholders s
+WHERE sf.sf_id = ftsodc.flow_id
+	AND s.id = ftsodc.stakeholder_id
+    AND ctf.flow_id = sf.sf_id
+    AND ctc.cc = ctf.cc_id
+    AND ce.id = ctc.ce
+    AND cc.id = ctc.cc
+    AND sf.flow_type = 'disbursed_funds'
+    AND sf.response_or_capacity = 'capacity'
+	AND sf.year >= 2014
+GROUP BY 
+    cc.fullname,
+    ce.fullname,
+    sf.year,
+    s.subcat,
+	s.name
+ORDER BY 1, 2, 5 desc,6 DESC;
