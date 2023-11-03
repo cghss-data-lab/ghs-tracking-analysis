636f613efb7e781a89a20281a8e3d5fe9d7349af
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2023-11-03 12:21:25 -0400

    Update analysis.sql

--- analyses/Flows by PHEIC/code/analysis.sql
+++ analyses/Flows by PHEIC/code/analysis.sql
@@ -1,2 +1,24 @@
 SELECT
-    1 AS "first_column"
+    f.name AS "Funder Name",
+    r.name AS "Recipient Name",
+    e.name AS "PHEIC Name",
+    SUM(sf.value) AS "Total Disbursed to Recipient"
+FROM
+    stakeholders r
+    JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON ftstdc.stakeholder_id = r.id
+    JOIN simple_flows sf ON sf.sf_id = ftstdc.flow_id
+    JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON ftsodc.flow_id = ftstdc.flow_id
+    JOIN stakeholders f ON f.id = ftsodc.stakeholder_id
+    JOIN events_to_flows etf ON etf.flow_id = sf.sf_id
+    JOIN events e ON e.id = etf.event_id
+WHERE
+    sf.flow_type = 'disbursed_funds'
+    AND sf.response_or_capacity = 'response'
+    AND r.subcat = 'country'
+    AND sf.year >= 2014
+GROUP BY
+    r.name,
+    f.name,
+    e.name
+ORDER BY
+    "Total Disbursed to Recipient" DESC;
