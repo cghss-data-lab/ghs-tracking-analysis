a909a27f31de3e03fc99e0d4e590444253453739
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2023-11-08 15:13:11 -0500

    Update analysis.sql

--- analyses/Core capacity recipients by WHO region/code/analysis.sql
+++ analyses/Core capacity recipients by WHO region/code/analysis.sql
@@ -1,2 +1,28 @@
-SELECT
-    1 AS "first_column"
+WITH top_level_stakeholder_ids AS (
+         SELECT DISTINCT children_to_parents_direct_credit.parent_id
+           FROM children_to_parents_direct_credit
+          WHERE NOT (children_to_parents_direct_credit.parent_id IN ( SELECT DISTINCT children_to_parents_direct_credit_1.child_id
+                   FROM children_to_parents_direct_credit children_to_parents_direct_credit_1
+                  WHERE children_to_parents_direct_credit_1.parent_id <> children_to_parents_direct_credit_1.child_id))
+        ), results AS (
+         SELECT s.region_who AS "Recipient WHO Region",
+            c.fullname AS "Core capacity",
+            c.name AS "Core capacity code",
+            sum(sf.value) AS "Total disbursed (nominal USD)"
+           FROM stakeholders s
+             JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON ftstdc.stakeholder_id = s.id
+             JOIN simple_flows sf ON sf.sf_id = ftstdc.flow_id
+             JOIN ccs_to_flows ccstf ON ccstf.flow_id = sf.sf_id
+             JOIN core_capacities c ON c.id = ccstf.cc_id
+          WHERE sf.flow_type = 'disbursed_funds'::flow_types 
+                 AND sf.response_or_capacity = 'capacity'
+                 AND (s.id IN ( SELECT top_level_stakeholder_ids.parent_id
+                   FROM top_level_stakeholder_ids))
+          GROUP BY c.fullname, c.name, s.region_who
+          ORDER BY c.fullname, (sum(sf.value)) DESC
+        )
+ SELECT results."Recipient WHO Region",
+    results."Core capacity",
+    results."Core capacity code",
+    results."Total disbursed (nominal USD)"
+   FROM results
