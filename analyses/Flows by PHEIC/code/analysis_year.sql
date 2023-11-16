a0f8c7ae5716f861575778539ef47b35768bb8d1
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2023-11-16 16:41:21 -0500

    Create analysis_year.sql

---
+++ analyses/Flows by PHEIC/code/analysis_year.sql
@@ -0,0 +1,25 @@
+SELECT
+    f.name AS "Funder Name",
+    r.name AS "Recipient Name",
+    sf.year as "Year",
+    e.name AS "PHEIC Name",
+    SUM(sf.value) AS "Total Disbursed to Recipient"
+FROM
+    stakeholders r
+    JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON ftstdc.stakeholder_id = r.id
+    JOIN simple_flows sf ON sf.sf_id = ftstdc.flow_id
+    JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON ftsodc.flow_id = ftstdc.flow_id
+    JOIN stakeholders f ON f.id = ftsodc.stakeholder_id
+    JOIN events_to_flows etf ON etf.flow_id = sf.sf_id
+    JOIN events e ON e.id = etf.event_id
+WHERE
+    sf.flow_type = 'disbursed_funds'
+    AND sf.response_or_capacity = 'response'
+GROUP BY
+    sf.year,
+	r.name,
+    f.name,
+    e.name
+ORDER by
+	"Year" DESC,
+    "Total Disbursed to Recipient" DESC;
